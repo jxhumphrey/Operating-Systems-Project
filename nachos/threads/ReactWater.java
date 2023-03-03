aecf9ebbb0e030023510cd297c73865afc8956fa
jusroy <jroy1@laurentian.ca>
2023-03-03 12:35:11 -0500

    Change reactWater implementation
    Implemented in a way that all threads wait for the appropriate amount of atoms to return.
    
    For the test I made an oxygen thread and 2 hydrogen threads to make the water molecules.
    
    Comments to the reactwater class and communicator class will come soon

--- nachos/threads/ReactWater.java
+++ nachos/threads/ReactWater.java
@@ -8,21 +8,25 @@ public class ReactWater {
     private LinkedList<Condition2> hydrogen;
     private LinkedList<Condition2> oxygen;
     
-    public ReactWater(Lock lock){
-        this.lock = lock;
+    public ReactWater(){
+        this.lock = new Lock();
         hydrogen = new LinkedList<>();
         oxygen = new LinkedList<>();
     }
     
     public void hReady(){
         lock.acquire();
+        System.out.println("Adding one hydrogen");
         if (oxygen.isEmpty() || hydrogen.isEmpty()){
-            hydrogen.addFirst(new Condition2(lock));
-            hydrogen.getFirst().sleep();
+            Condition2 cond = new Condition2(lock);
+            hydrogen.add(cond);
+            cond.sleep();
         
         } else {
             oxygen.getFirst().wake();
+            oxygen.removeFirst();
             hydrogen.getFirst().wake();
+            hydrogen.removeFirst();
             this.MakeWater();
         }
         lock.release();
@@ -30,101 +34,77 @@ public class ReactWater {
     
     public void oReady(){
         lock.acquire();
+        System.out.println("Adding one oxygen");
         if (hydrogen.size() < 2){
-            oxygen.addFirst(new Condition2(lock));
-            oxygen.getFirst().sleep();
+            Condition2 cond = new Condition2(lock);
+            oxygen.add(cond);
+            cond.sleep();
         
         } else{
             hydrogen.getFirst().wake();
+            hydrogen.removeFirst();
             hydrogen.getFirst().wake();
+            hydrogen.removeFirst();
             this.MakeWater();
         }
         lock.release();
     }
     
     private void MakeWater(){
-        lock.acquire();
-        oxygen.removeFirst();
-        hydrogen.removeFirst();
-        hydrogen.removeFirst();
         System.out.println("Water was made!!");
-        lock.release();
     }
 
 
-	public static void selfTest()
-	{
+    public static void selfTest(){
         System.out.println("-----------------------Testing ReactWater Start----------------------------\n");
 
-		final Lock selfTestLock = new Lock();
-		final ReactWater reaction = new ReactWater(selfTestLock);
-		KThread waterTest1 = new KThread(new Runnable(){
+        final ReactWater reaction = new ReactWater();
+        KThread hydrogenTest1 = new KThread(new Runnable(){
 
             public void run(){
-                selfTestLock.acquire();
-                System.out.println("Test 1: 1 Hydrogen Atom and 1 Oxygen Atom");
-                reaction.oReady();
-                reaction.hReady(); 
-                System.out.println("Test 1: Water wasn't made because there is only " + reaction.hydrogen.size() + " hydrogen atom and " + reaction.oxygen.size() + " oxygen atom."); 
-				System.out.println("Test 1: Complete\n");
-				selfTestLock.release();
-				
-        }});
-		waterTest1.fork();
-        waterTest1.join();
-		
-		KThread waterTest2 = new KThread(new Runnable(){
-
-            public void run(){
-                selfTestLock.acquire();
-                System.out.println("Test 2: Add 1 hydrogen atom");
-                reaction.hReady();    
-                System.out.println("Test 2: Water should have been made with " + reaction.hydrogen.size() + " hydrogen atoms left over and " + reaction.oxygen.size() + " oxygen atoms left over."); 
-				System.out.println("Test 2: Complete\n");
-				selfTestLock.release();
-			}
+                
+                for (int i = 0; i < 5; i++){
+                    reaction.hReady();
+                }
+            }
         });
-		waterTest2.fork();
-		waterTest2.join();
-		
-		KThread waterTest3 = new KThread(new Runnable(){
+        
+        KThread hydrogenTest2 = new KThread(new Runnable(){
 
             public void run(){
-                selfTestLock.acquire();
-                System.out.println("Test 3: Add 4 Hydrogen Atoms");
-                reaction.hReady();  
-                reaction.hReady(); 
-                reaction.hReady(); 
-                reaction.hReady();   
-                System.out.println("Test 3: Water wasn't made because there is only " + reaction.hydrogen.size() + " hydrogen atom and " + reaction.oxygen.size() + " oxygen atom."); 
-				System.out.println("Test 3: Complete\n");
-				selfTestLock.release();
-			}
+                
+                for (int i = 0; i < 5; i++){
+                    reaction.hReady();
+                }
+            }
+        });
+        
+        KThread oxygenTest = new KThread(new Runnable(){
+
+            public void run(){
+                
+                for (int i = 0; i < 5; i++){
+                    reaction.oReady();
+                }
+            }
         });
 
-		waterTest3.fork();
-        waterTest3.join();
-		
-		
-		KThread waterTest4 = new KThread(new Runnable(){
+        hydrogenTest1.fork();
+        hydrogenTest2.fork();
+        oxygenTest.fork();
+        try {
+            hydrogenTest1.join();
+            hydrogenTest2.join();
+            oxygenTest.join();
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
-            public void run(){
-                selfTestLock.acquire();
-                System.out.println("Test 4: Add 2 Oxygen Atoms");
-                reaction.oReady();
-                reaction.oReady();    
-                System.out.println("Test 4: Water should have been made twice with " + reaction.hydrogen.size() + " hydrogen atoms and " + reaction.oxygen.size() + " oxygen atoms left over."); 
-				System.out.println("Test 4: Complete\n");
-				selfTestLock.release();
-			}
-        });
 
-		waterTest4.fork();
-		waterTest4.join();
+    System.out.println("-----------------------Testing ReactWater  END----------------------------");
 
-        System.out.println("-----------------------Testing ReactWater  END----------------------------");
 
-		
-	}
+    }
 
 } 
