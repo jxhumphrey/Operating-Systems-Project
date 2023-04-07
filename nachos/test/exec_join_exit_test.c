eaa40c6c575d67d41512d694cb7b5a85b3256088
andrewtriumph <123575747+andrewtriumph@users.noreply.github.com>
2023-04-07 12:14:17 -0400

    task 3 test program done

--- nachos/test/exec_join_exit_test.c
+++ nachos/test/exec_join_exit_test.c
@@ -2,11 +2,19 @@
 #include "stdio.h"
 
 int main(){
-    char *argv2[] = {"arg1", "arg2"};
-    int processID1 = exec("echo.coff", 2, argv);
-    char *argv[] = {""};
-    int processID2 = exec("halt.coff", 1, argv);
-    join(processID1, 1);
-    join(processID1, 1);
-    std::cout << "exit code: " << exit(1);
+    char *argv1[] = {"arg1", "arg2"};
+    int processID1 = exec("echo.coff", 2, argv1);
+    char *argv2[] = {""};
+    int processID2 = exec("halt.coff", 1, argv2);
+    int childStatus1;
+    int childStatus2;
+    if (join(processID1, &childStatus1) == -1) {
+        printf("First join failed\n");
+        exit(1);
+    }
+    if (join(processID2, &childStatus2) == -1) {
+        printf("First join failed\n");
+        exit(1);
+    }
+    printf("child exit code 1: %d, child exit code 2: %d\n", childStatus1, childStatus2);
 }
